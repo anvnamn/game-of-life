a8b1303a21ec5185cd970002dd019aefc6ca864f
anvnamn <85112292+anvnamn@users.noreply.github.com>
2021-06-10 22:14:24 +0200

    Streamlined manually setting cell state Now it is possible to click and drag to set states of several cells more easily Fixes #6

--- Assets/Scripts/CellToggler.cs
+++ Assets/Scripts/CellToggler.cs
@@ -7,18 +7,31 @@ public class CellToggler : MonoBehaviour
     Ray ray;
     RaycastHit2D hit;
 
-    void Update()
+    void Start()
     {
-        if (Input.GetMouseButtonDown(0)) ToggleCellState();
+        StartCoroutine(DoRayCastsOnCells());
     }
 
-    void ToggleCellState()
+    IEnumerator DoRayCastsOnCells()
+    {
+        while (true)
+        {
+            if (Input.GetMouseButton(0)) SetCellState(true);
+            else if (Input.GetMouseButton(1)) SetCellState(false);
+            yield return new WaitForSeconds(0.025f); // Run this check 50 times per second
+        }
+    }
+
+    void SetCellState(bool state)
     {
         ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         hit = Physics2D.Raycast(ray.origin, ray.direction, Mathf.Infinity);
-        if (hit.collider.gameObject != null)
-        {
-            hit.collider.gameObject.GetComponent<Cell>().ToggleState();
+        if (hit)
+            {
+            if (hit.collider.gameObject != null)
+            {
+                hit.collider.gameObject.GetComponent<Cell>().SetState(state);
+            }
         }
     }
 }
